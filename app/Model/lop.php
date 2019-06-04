85d9a22aba76e4e61588d15262947b47818b373d
sondang9961 <47907341+sondang9961@users.noreply.github.com>
2019-06-04 09:30:54 +0700

    abc

---
+++ app/Model/lop.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class Lop extends Model
+{
+	static function get_all()
+	{
+		$array_lop= DB::select ("select * from lop join khoa_hoc on lop.ma_khoa_hoc = khoa_hoc.ma_khoa_hoc");
+		return $array_lop;
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,3 @@
 <?php
 
-//Route::get('sinh_vien/view_one/{ma_sinh_vien}',"sinh_vien_controller@view_one"); 
-//Route::get('nhap',"a_cong_b_controller@nhap");
-//Route::post('xu_ly_nhap','a_cong_b_controller@xu_ly_nhap')->name('xu_ly_nhap');
 Route::get('trang_chu',"trang_chu_controller@trang_chu");
