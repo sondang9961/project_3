6a09b1920331a1a7c983144e898d1dcc0369d4ca
sondang9961 <47907341+sondang9961@users.noreply.github.com>
2019-06-21 17:23:51 +0700

    up

--- resources/views/view_thong_tin_ca_nhan.blade.php
+++ resources/views/view_thong_tin_ca_nhan.blade.php
@@ -1,29 +1,4 @@
 @extends('layer.master')
-@push('css')
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="img/favicon.ico">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-	<title>Login</title>
-
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-
-
-    <!-- Bootstrap core CSS     -->
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" />
-   
-
-    <!--  Light Bootstrap Dashboard core CSS    -->
-    <link href="{{asset('css/light-bootstrap-dashboard.css')}}" rel="stylesheet" />
-
-
-
-    <!--     Fonts and icons     -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="css/pe-icon-7-stroke.css" rel="stylesheet" />
-@endpush
 @section('content')
 	<div class="main-content">
         <div class="container-fluid">
@@ -48,7 +23,7 @@
 			                        <div class="col-md-3">
 			                            <div class="form-group">
 			                                <label>Tên hiển thị</label>
-			                                <input type="text" name="ten_giao_vu" class="form-control" placeholder="Username" value="{{$array_giao_vu->ten_giao_vu}}">
+			                                <input type="text" name="ten_giao_vu" class="form-control" placeholder="Username" value="{{$array_giao_vu[0]->ten_giao_vu}}">
 			                            </div>
 			                        </div>
 			                    </div>
@@ -56,7 +31,7 @@
 			                        <div class="col-md-4">
 			                            <div class="form-group">
 			                                <label for="exampleInputEmail1">Email</label>
-			                                <input type="email" name="email" class="form-control" value="{{$array_giao_vu->email}}">
+			                                <input type="email" name="email" class="form-control" value="{{$array_giao_vu[0]->email}}">
 			                            </div>
 			                        </div>			                    
 								</div>
@@ -64,7 +39,7 @@
 			                        <div class="col-md-4">
 			                            <div class="form-group">
 			                                <label for="exampleInputEmail1">Số điện thoại</label>
-			                                <input type="text" name="sdt" class="form-control" value="{{$array_giao_vu->sdt}}">
+			                                <input type="text" name="sdt" class="form-control" value="{{$array_giao_vu[0]->sdt}}">
 			                            </div>
 			                        </div>			                    
 								</div>
@@ -72,7 +47,7 @@
 			                        <div class="col-md-12">
 			                            <div class="form-group">
 			                                <label>Địa chỉ</label>
-			                                <input type="text" name="dia_chi" class="form-control" placeholder="Home Address" value="{{$array_giao_vu->dia_chi}}">
+			                                <input type="text" name="dia_chi" class="form-control" placeholder="Home Address" value="{{$array_giao_vu[0]->dia_chi}}">
 			                            </div>
 			                        </div>
 			                    </div>
@@ -87,15 +62,3 @@
 	</div>
 
 @endsection
-@push('js')
-  <!--   Core JS Files  -->
-    <script src="{{asset('js/jquery.min.js')}}" type="text/javascript"></script>
-    <script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
-    <script src="{{asset('js/perfect-scrollbar.jquery.min.js')}}" type="text/javascript"></script>
-
-
-    <!-- Light Bootstrap Dashboard Core javascript and methods -->
-	<script src="{{asset('js/light-bootstrap-dashboard.js')}}"></script>
-
-
-@endpush
